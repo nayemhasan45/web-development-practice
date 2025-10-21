e398a378d53de07239d0ebb6aa202287523492e2
nayemhasan45 <nayemhasan75907@gmail.com>
2025-10-21 21:05:00 +0300

    server side setup for coffe house

---
+++ coffe_house/coffe_house_server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app= express();
+const port = process.env.PORT || 3000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("hello from coffe server");
+})
+
+app.listen(port,()=>{
+    console.log('coffe server is running on port : ',port);
+})
